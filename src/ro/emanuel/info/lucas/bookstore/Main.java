5aee685389580807d7710bce0b979a417c43b1d3
Lucas Cruceru <Lucas Cruceru>
2017-04-28 09:47:58 +0300

    JDBC CRUD functions for orders, products, orderDetails and customers

---
+++ src/ro/emanuel/info/lucas/bookstore/Main.java
@@ -0,0 +1,32 @@
+package ro.emanuel.info.lucas.bookstore;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//testing products
+		//CRUDProducts.insert(4, "sample", 50, 1, "DESC");
+		//CRUDProducts.update(3, "1984", 40, 1, "a very good book");
+		//CRUDProducts.delete(4);
+		//CRUDProducts.selectAll();
+		
+		//testing customers
+		//CRUDCustomers.insert(3, "Lucas", "Cruceru", "lucascruceru@gmail.com", "adresa", "Oradea", "Bihor", "Romania" );
+		//CRUDCustomers.update(2, "Ion", "popa" , "ionpopa@gmail.com", "adresa", "Cluj-Napoca", "Cluj", "Romania");
+		//CRUDCustomers.delete(3);
+		//CRUDCustomers.selectAll();
+		
+		//testing orders
+		//CRUDOrders.insert(3, "12.07.2017", 120, 1);
+		//CRUDOrders.update(2, "13.07.2017", 200, 2);
+		//CRUDOrders.delete(3);
+		//CRUDOrders.selectAll();
+		
+		//testing orderDetails
+		//CRUDOrderDetails.insert(3, 1, 2);
+		//CRUDOrderDetails.update(2, 2, 1);
+		//CRUDOrderDetails.delete(3);
+		//CRUDOrderDetails.selectAll();
+		
+	}
+
+}
